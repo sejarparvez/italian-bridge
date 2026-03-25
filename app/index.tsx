c0b8b4cebeb325b1c334926b94c139657b3bb301
Sejar Parvez <sejarparvez@yahoo.com>
2026-03-25 20:36:32 +0600

    clean up our project

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+	return (
+		<View className="flex-1 items-center justify-center bg-white dark:bg-black">
+			<Text className="text-black dark:text-white text-2xl font-bold">
+				this is the home screen
+			</Text>
+		</View>
+	);
+}
