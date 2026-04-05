694489118e2bcfd2fc8951ef4f0e004c68aaafcc
Sejar Parvez <sejarparvez@yahoo.com>
2026-04-05 16:24:16 +0600

    added game screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,13 @@
 import { Stack } from 'expo-router';
+import * as ScreenOrientation from 'expo-screen-orientation';
+import { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 
 export default function Layout() {
+  useEffect(() => {
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+  }, []);
+
   return (
     <>
       <Stack screenOptions={{ headerShown: false }} />
